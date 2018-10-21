0e9942f2254123a22e355f4ddcea7f979b92629a
Jay Freeman <jayfreeman@Jays-MacBook-Pro.local>
2018-10-21 19:52:56 +0800

    further work

--- src/agents/IntentAgent.java
+++ src/agents/IntentAgent.java
@@ -1,6 +1,6 @@
 package agents;
-import hanabAI.*;
 
+import hanabAI.*;
 import java.util.Arrays;
 
 /**
@@ -9,8 +9,8 @@ import java.util.Arrays;
 public class IntentAgent implements Agent {
 
   class MindState {
-    public int certainRank;
-    public Colour certainColour;
+    public int knownRank;
+    public Colour knownColour;
     public int[][] state;
 
     public MindState(int[] spread) {
@@ -21,15 +21,16 @@ public class IntentAgent implements Agent {
       }
     }
 
-    public MindState(int[][] avaliable) {
-      //init MindState from avaliable cards
+    public MindState(int[][] potential) {
+      //init MindState from potential card space
       state = new int[5][];
       for (int i = 0; i < 5; ++i) {
-        state[i] = Arrays.copyOf(avaliable[i], avaliable[i].length);
+        state[i] = Arrays.copyOf(potential[i], potential[i].length);
       }
     }
 
     public void rankHint(int rank) {
+      knownRank = rank;
       for (int i = 0; i < 5; ++i) {
         for (int j = 0; j < 5; ++j) {
           if (j != rank - 1) {
@@ -40,6 +41,7 @@ public class IntentAgent implements Agent {
     }
 
     public void colourHint(Colour colour) {
+      knownColour = colour;
       for (int i = 0; i < 5; ++i) {
         for (int j = 0; j < 5; ++j) {
           if (i != colour.ordinal()) {
@@ -49,8 +51,8 @@ public class IntentAgent implements Agent {
       }
     }
 
-    public void remove(Card card) {
-      state[card.getColour().ordinal()][card.getValue()] -= 1;
+    public void remove(int colour, int rank) {
+      if (state[colour][rank] > 0) state[colour][rank]--;
     }
   }
 
@@ -58,31 +60,43 @@ public class IntentAgent implements Agent {
     public int id;
     public String name;
     public boolean trustworthy = true;
+    public boolean intentional = true;
     public Card[] hand;
     public MindState[] mind;
+    public int[][] potential;
 
-    public Player(int playerID, int agentID, int[][] cardsAvaliable, State state) {
-      this.id = playerID;
-      this.name = state.getPlayers()[id];
+    public Player(int playerID, int agentID, int handSize, int[][] cardsAvaliable, State state) {
+      id = playerID;
+      name = state.getPlayers()[id];
       if (id != agentID) {
-        this.hand = state.getHand(id); // cant see own hand
+        hand = state.getHand(id); // cant see own hand
+      }
+
+      // record potential
+      potential = new int[5][];
+      for (int i = 0; i < 5; ++i) {
+        potential[i] = Arrays.copyOf(cardsAvaliable[i], NUM_RANKS);
       }
 
       // init default mindstates
-      mind = new MindState[IntentAgent.handSize];
-      for (int i = 0; i < IntentAgent.handSize; i++) {
-        mind[i] = new MindState(cardsAvaliable);
+      mind = new MindState[handSize];
+      for (int i = 0; i < handSize; i++) {
+        mind[i] = new MindState(potential);
       }
     }
-    
+
     public void removeFromMind(Card card) {
+      int colour = card.getColour().ordinal();
+      int rank = card.getValue();
+
+      potential[colour][rank]--;
       for (MindState mindState : mind) {
-        mindState.remove(card);
+        mindState.remove(colour, rank);
       }
     }
 
-    public void update(Action action) {
-
+    public void resetCardState(int handPos) {
+      mind[handPos] = new MindState(potential);
     }
   }
 
@@ -92,19 +106,21 @@ public class IntentAgent implements Agent {
 
   private static int handSize;
   private static int numPlayers;
-  private static int[][] cardsAvaliable;
+  private static int board;
 
+  private int[][] cardsAvaliable;
   private Player[] players;
+  private Player self;
   private boolean firstAction = true;
   private int currentOrder;
   private State currentState;
-  private Player self;
+
 
   /**
    * Default constructor.
    */
   public IntentAgent() {}
-  
+
   /**
    * Initialises agent on first call.
    */
@@ -123,20 +139,20 @@ public class IntentAgent implements Agent {
 
     selfID = currentState.getNextPlayer();
     currentOrder = currentState.getOrder();
-    
-    // init avaliable cards i.e. any not discarded or played
+
+    // init avaliable cards i.e. any not yet discarded or played
     cardsAvaliable = new int[NUM_COLOURS][];
     for (int i = 0; i < NUM_RANKS; ++i) {
       cardsAvaliable[i] = Arrays.copyOf(CARD_SPREAD, NUM_RANKS);
     }
 
-    // init players visible/mental states
+    // init a player records
     players = new Player[numPlayers];
     for (int i = 0; i < numPlayers; ++i) {
-      players[i] = new Player(i, selfID, cardsAvaliable, currentState);
+      players[i] = new Player(i, selfID, handSize, cardsAvaliable, currentState);
     }
 
-    // find first state by stepping back to the first player
+    // find first state by stepping back to the first players turn
     firstState = currentState;
     for (int i = 0; i < self.id; ++i) {
       firstState = firstState.getPreviousState();
@@ -162,28 +178,51 @@ public class IntentAgent implements Agent {
 
   private State getPastStateByOrder(int targetOrder) {
     State target = currentState;
+
     while (target.getOrder() != targetOrder) {
       target = target.getPreviousState();
     }
     return target;
   }
 
-  private void applyDiscard(Action action, State result) {
-    // decrease card counts
-    Player actor = players[action.getPlayer()];
-    Card card = actor.hand[action.getCard()];
+  private void applyDiscard(Action action, Player actor, State result) {
+    int handPos = action.getCard();
+    Card card = actor.hand[handPos];
+    
+    // decrease avaliablity counts
     cardsAvaliable[card.getColour().ordinal()][card.getValue()] -= 1;
 
-    // replace if avaliable
-    pickupCard(action, result);
+    // pickup if avaliable
+    pickupCard(actor, handPos, result);
   }
 
-  private void pickupCard(Action action, State result) {
-    // actor picks up a new card
-    Player actor = players[action.getPlayer()];
-    actor.hand = result.getHand(actor.id);
-    // new card is unknown
+  private void applyPlay(Action action, Player actor, State result) {
+    
+  }
 
+  private void pickupCard(Player actor, int handPos, State result) {
+    Card card;
+    
+    // if game is over we can't pick up
+    if (result.gameOver()) {
+      return;
+    }
+
+    // cant manage our own hand
+    if (actor == self) {
+      return;
+    }
+
+    // otherwise actor picks up a new card
+    actor.hand = result.getHand(actor.id);
+    actor.resetMind(handPos);
+
+    // other players see the new card
+    card = actor.hand[handPos];
+    for (Player player : players) {
+      if (player == actor) continue;
+      player.removeFromMind(card);
+    }
   }
 
   private void applyAction(Action action, Player actor) {
@@ -193,16 +232,16 @@ public class IntentAgent implements Agent {
     //switch on type of action
     switch (action.getType()) {
       case DISCARD:
-        applyDiscard(action, result);
+        applyDiscard(action, actor, result);
         break;
       case HINT_COLOUR:
-        applyColourHint(action);
+        applyColourHint(action, actor, result);
         break;
       case HINT_VALUE:
-        applyRankHint(action);
+        applyRankHint(action, actor, result);
         break;
       case PLAY:
-        applyPlay(action);
+        applyPlay(action, actor, result);
         break;
     }
   }
