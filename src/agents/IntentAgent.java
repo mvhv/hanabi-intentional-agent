6b1cc8459c2849200c7fcaca2463c7303490b911
Jesse Wyatt <wyattjrw@gmail.com>
2018-10-24 02:25:52 +0800

    Partial JavaDocs
    Documentation incomplete, but commiting changes due to power loss.

--- src/agents/IntentAgent.java
+++ src/agents/IntentAgent.java
@@ -9,31 +9,61 @@ import java.util.Stack;
 import java.lang.Math;
 
 /**
- * An interface for representing the strategy of a player in Hanabi
- * */
+ * An implementation of an intentional agent for playing Hanabi.
+ * The agent implements a modified Van den Bergh ruleset, with the
+ * additon of tracking the mind states of all players, and attempting
+ * to rate their "intentionality". If an opponent's actions are considered
+ * to be "intentional", then hints from that agent are considered
+ * more liberally when analysing plays.
+ * 
+ * @author Jesse Wyatt (20756971)
+ **/
 public class IntentAgent implements Agent {
 
   private static final int NUM_COLOURS = Colour.values().length;
   private static final int NUM_RANKS = 5;
   private static final int[] RANK_SPREAD = {3, 2, 2, 2, 1};
 
+  /**The number of cards per hand */
   private static int handSize;
+  /**The number of players in the game */
   private static int numPlayers;
 
+  /**Frequencies for cards that haven't been discarded or played */
   private int[][] allUnplayed;
+  /**Mind state models for all players */
   private Player[] players;
+  /**Mind state model for this agent */
   private Player self;
+  /**True if the agent is playing it's first action */
   private boolean firstAction = true;
-  private int currentOrder;
+  /**The current game state */
   private State currentState;
+  /**The order number of the current game state */
+  private int currentOrder;
 
+  /**
+   * A class representing all information a player knows about a
+   * single card in it's own hand.
+   */
   class MindState {
+    /**True if the player knows the rank of this card */
     private boolean knowRank = false;
+    /**True if the player knows the colour of this card */
     private boolean knowColour = false;
+    /**The zero-based rank of this card if known */
     private int rank = -1;
+    /**The colour of this card if known */
     private Colour colour = null;
+    /**Known frequencies of all possible values for this card */
     private int[][] state;
 
+    /**
+     * Constructs an instance of the MindState model for this card using
+     * the existing knowledge of the player holding it.
+     * @param potential the frequencies of all possible values avaliable to
+     * the player holding this card.
+     */
     public MindState(int[][] potential) {
       //init MindState from potential card space
       state = new int[5][];
@@ -42,6 +72,11 @@ public class IntentAgent implements Agent {
       }
     }
 
+    /**
+     * Updates model of player's knowledge of this card after receiving
+     * a rank hint.
+     * @param rank the zero-based rank hinted
+     */
     public void rankHint(int rank) {
       knowRank = true;
       this.rank = rank;
@@ -52,8 +87,14 @@ public class IntentAgent implements Agent {
           }
         }
       }
+      updateKnown();
     }
 
+    /**
+     * Updates model of player's knowledge of this card after receiving
+     * a colour hint.
+     * @param colour the colour hinted
+     */
     public void colourHint(Colour colour) {
       knowColour = true;
       this.colour = colour;
@@ -64,8 +105,18 @@ public class IntentAgent implements Agent {
           }
         }
       }
+      updateKnown();
     }
 
+    /**
+     * Checks to see if the current card is useless based on player knowledge
+     * of card frequencies and the state of board. Returns true if the
+     * card is useless, false otherwise. 
+     * @param board an array describing the values of the top ranking played
+     * card for each clour
+     * @param allUnplayed the player's knowledge of unplayed card frequencies
+     * @return True if the card is useless, false otherwise.
+     */
     public boolean isUseless(int[] board, int[][] allUnplayed) {
       int col, top;
       int count = 0;
@@ -94,12 +145,19 @@ public class IntentAgent implements Agent {
       }
     }
 
+    /**
+     * Checks card knowledge and records rank and colour values if they
+     * are certain.
+     */
     private void updateKnown() {
       int[] colAggs = new int[NUM_COLOURS];
       int[] rankAggs = new int[NUM_RANKS];
       int count, c;
       int trueRank = -1;
       Colour trueColour = null;
+
+      // skip check if already known
+      if (knowRank && knowColour) return;
 
       // count row and colour aggregates
       for (Colour col : Colour.values()) {
@@ -110,65 +168,91 @@ public class IntentAgent implements Agent {
         }
       }
 
-      // count nonzero colour aggregates
-      count = 0;
-      for (Colour col : Colour.values()) {
-        c = col.ordinal();
-        if (colAggs[c] > 0) {
-          count++;
-          trueColour = col;
+      if (!knowColour) {
+        // count nonzero colour aggregates
+        count = 0;
+        for (Colour col : Colour.values()) {
+          c = col.ordinal();
+          if (colAggs[c] > 0) {
+            count++;
+            trueColour = col;
+          }
         }
-      }
-      // if exactly one nonzero agg then we know true colour
-      if (count == 1) { 
-        this.knowColour = true;
-        this.colour = trueColour;
+        // if exactly one nonzero agg then we know true colour
+        if (count == 1) { 
+          this.knowColour = true;
+          this.colour = trueColour;
+        }
       }
       
-      // count nonzero rank aggregates
-      count = 0;
-      for (int r = 0; r < NUM_RANKS; r++) {
-        if (rankAggs[r] > 0) {
-          count++;
-          trueRank = r;
+      if (!knowRank) {
+        // count nonzero rank aggregates
+        count = 0;
+        for (int r = 0; r < NUM_RANKS; r++) {
+          if (rankAggs[r] > 0) {
+            count++;
+            trueRank = r;
+          }
         }
-      }
-      // if exactly one nonzero agg then we know true rank
-      if (count == 1) {
-        this.knowRank = true;
-        this.rank = trueRank;
+        // if exactly one nonzero agg then we know true rank
+        if (count == 1) {
+          this.knowRank = true;
+          this.rank = trueRank;
+        }
       }
     }
 
+    /**
+     * Removes a card from the record of potential states.
+     * @param colour the integer colour ordinal of the card 
+     * @param rank the rank of the card
+     */
     public void remove(int colour, int rank) {
       if (state[colour][rank] > 0) {
         state[colour][rank]--;
       }
-      // update if removal tells us new info
       updateKnown();
     }
 
-    // dummy method to fix vscode debugging issues
+    // unused accessor method to fix vscode debug-engine issues
     public int getRank() {
       return this.rank;
     }
 
-    // dummy method to fix vscode debugging issues
+    // unused accessor method to fix vscode debug-engine issues
     public Colour getColour() {
       return this.colour;
     }
   }
 
+  /**Enum for holding hint types */
   enum HintType {COLOUR, RANK};
 
+  /**
+   * A class representing all information reguarding a single hint. Packaged
+   * for use in a list tracking hint history.
+   */
   class Hint {
+    /**The player receiving the hint */
     private Player hintee;
+    /**The player providing the hint */
     private Player hinter;
+    /**The cards included in the hint */
     private boolean[] targets;
+    /**The type of hint (COLOUR or RANK) */
     private HintType type;
+    /**The rank of the hint */
     private int rank;
+    /**The colour of the hint */
     private Colour colour;
 
+    /**
+     * Constructs an instance for rank hints.
+     * @param rank the rank hinted
+     * @param targets the cards hinted
+     * @param hinter the player giving the hint
+     * @param hintee the player recieving the hint
+     */
     public Hint(int rank, boolean[] targets, Player hinter, Player hintee) {
       type = HintType.RANK;
       this.rank = rank;
@@ -177,6 +261,13 @@ public class IntentAgent implements Agent {
       this.hintee = hintee;
     }
 
+    /**
+     * Constructs an instance for colour hints.
+     * @param colour the colour hinted
+     * @param targets the cards hinted
+     * @param hinter the player giving the hint
+     * @param hintee the player receiving the hint
+     */
     public Hint(Colour colour, boolean[] targets, Player hinter, Player hintee) {
       type = HintType.COLOUR;
       this.colour = colour;
@@ -185,6 +276,10 @@ public class IntentAgent implements Agent {
       this.hintee = hintee;
     }
 
+    /**
+     * Checks a colour hint for validity. True if valid, false otherwise. 
+     * @return true if valid, false otherwise
+     */
     private boolean colourValid() {
       for (int i = 0; i < handSize; i++) {
         if (targets[i]) {
@@ -196,6 +291,10 @@ public class IntentAgent implements Agent {
       return true;
     }
 
+    /**
+     * Checks a rank hint for validity. True if valid, false otherwise. 
+     * @return true if valid, false otherwise
+     */
     private boolean rankValid() {
       for (int i = 0; i < handSize; i++) {
         if (targets[i]) {
@@ -207,6 +306,10 @@ public class IntentAgent implements Agent {
       return true;
     }
 
+    /**
+     * Checks a hint for validity. True if valid, false otherwise. 
+     * @return true if valid, false otherwise
+     */   
     public boolean isValid() {
       if (type == HintType.COLOUR) {
         return this.colourValid();
@@ -216,17 +319,37 @@ public class IntentAgent implements Agent {
     }
   }
 
+  /**
+   * A class for tracking all information about players. Including a 
+   * model of the potential mind state of other players.
+   */
   class Player {
+    /**The player's order in the game */
     private int id;
+    /**The player's name */
     private String name;
+    /**True if the player has not made any mistakes */
     private boolean trustworthy = true;
+    /**Estimated probability of player's hints being "intentional" */
     private double hintIntentional = 0.5;
     //private double playIntentional = 0.5;
+    /**The player's actual hand if visible */
     private Card[] hand;
+    /**The player's internal */
     private MindState[] mind;
+    /**The player's knowledge of card frequencies */
     private int[][] potential;
+    /**A list of all hints given to the player */
     private List<Hint> hints;
 
+    /**
+     * Constructs an instance of the player model.
+     * @param playerID the id of the player to be modeled
+     * @param agentID the id of the player constructing th model
+     * @param handSize the numer of cards per hand
+     * @param allUnplayed the initial card distribution
+     * @param state the initial game state
+     */
     public Player(int playerID, int agentID, int handSize, int[][] allUnplayed, State state) {
       id = playerID;
       name = state.getPlayers()[id];
@@ -250,6 +373,7 @@ public class IntentAgent implements Agent {
       hints = new ArrayList<Hint>();
     }
 
+    /** */
     private void removeFromMind(Card card) {
       int colour = card.getColour().ordinal();
       int rank = getRank(card);
